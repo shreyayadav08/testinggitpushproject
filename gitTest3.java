41ed378da791852690a0b744c28317df3abbd3a3
shreyayadav08 <shreyapabbala@gmail.com>
2026-08-21 15:25:52 +0530

    my third commit

---
+++ gitTest3.java
@@ -0,0 +1,10 @@
+public class demo{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int result=a+b;
+        System.out.println("result is : " + result);
+
+
+    }
+}
